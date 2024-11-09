0c63d27ea466b324a611dfc1fdebd0c918d08b5a
Nicolas Leroux <hertford.fr@gmail.com>
2024-11-09 19:11:20 +0100

    style: rename import paths with @plan, add semicolons and do stuff with blank lines

--- src/app/plan-systems-tests/models/classes/rectangle.class.spec.ts
+++ src/app/plan-systems-tests/models/classes/rectangle.class.spec.ts
@@ -1,16 +1,18 @@
-import { CartesianCoordinate } from "../../../plan-systems/models/classes/cartesian-coordinate.class";
-import { Rectangle } from "../../../plan-systems/models/classes/rectangle.class";
+import { CartesianCoordinate } from "@plan/models/classes/cartesian-coordinate.class";
+import { Rectangle } from "@plan/models/classes/rectangle.class";
 
 describe('Rectangle', () => {
     const rect = new Rectangle(10, 10);
 
     it('should be 100 area for rectange(10, 10)', () => {
         expect(rect.getArea()).toEqual(100);
-    })
+    });
+
     it('should be 40 perimeter for rectange(10, 10)', () => {
         expect(rect.getPerimeter()).toEqual(40);
-    })
+    });
+
     it('should be (5, 5) center for rectange(10, 10)', () => {
         expect(rect.getCenter()).toEqual(new CartesianCoordinate(5, 5));
-    })
-})
+    });
+});
