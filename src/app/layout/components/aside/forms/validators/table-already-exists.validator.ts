d51bd7548d0d57856a5358f0f251fa0ef6aa7711
Nicolas Leroux <hertford.fr@gmail.com>
2024-11-09 06:37:30 +0100

    feature: add a async validator ti check if table already exists

---
+++ src/app/layout/components/aside/forms/validators/table-already-exists.validator.ts
@@ -0,0 +1,20 @@
+import { inject, Injectable } from "@angular/core";
+import { AbstractControl, AsyncValidator, ValidationErrors } from "@angular/forms";
+import { McdTableService } from "../../../../../domain/mcd/services/table/mcd-table.service";
+import { catchError, first, map, Observable, of, switchMap, take } from "rxjs";
+
+@Injectable({ providedIn: 'root' })
+export class TableAlreadyEsistsValidator implements AsyncValidator {
+    tableService: McdTableService = inject(McdTableService);
+
+    validate(control: AbstractControl): Observable<ValidationErrors | null> {
+        return control.events.pipe(
+            take(1),
+            switchMap(() => this.tableService.$exists(control.value).pipe(
+                map((exists) => (exists ? { tableExists: true } : null)),
+                catchError(() => of(null)),
+                first()
+            ))
+        );
+    }
+}
