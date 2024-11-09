47f1904a08761240e4db2e740b20aaf57340f0e0
Nicolas Leroux <hertford.fr@gmail.com>
2024-11-09 18:56:08 +0100

    feat: add attribute exists  Validator

--- src/app/layout/components/aside/forms/table-form/table-form.component.ts
+++ src/app/layout/components/aside/forms/table-form/table-form.component.ts
@@ -1,11 +1,11 @@
 import { NgFor } from '@angular/common';
 import { Attribute, Component, inject } from '@angular/core';
-import { FormArray, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { McdTableService } from '../../../../../domain/mcd/services/table/mcd-table.service';
 import { McdTable } from '../../../../../domain/mcd/models/mcd-table.class';
 import { McdTableAttribute } from '../../../../../domain/mcd/models/mcd-table-attribute.type';
 import { FocusDirective } from '../../../../directives/focus.directive';
-import { TableAlreadyExistsValidator } from '../validators/table-already-exists/table-already-exists.validator';
+import { TableExistsValidator } from '../validators/table-exists/table-exists.validator';
 
 @Component({
   selector: 'app-table-form',
@@ -16,7 +16,7 @@ import { TableAlreadyExistsValidator } from '../validators/table-already-exists/
 })
 export class TableFormComponent {
   tableService: McdTableService = inject(McdTableService);
-  tableValidator: TableAlreadyExistsValidator = inject(TableAlreadyExistsValidator);
+  tableValidator: TableExistsValidator = inject(TableExistsValidator);
 
   formBuilder = inject(FormBuilder);
   tableForm = this.formBuilder.group({
@@ -30,7 +30,7 @@ export class TableFormComponent {
 
   addTableAttributes(): void {
     const attributesGroup = this.formBuilder.group({
-      attributeName: ['', [Validators.required]]
+      attributeName: ['', [Validators.required, this.attributeExistsValidator()]]
     });
     this.attributes.push(attributesGroup);
   }
@@ -45,5 +45,17 @@ export class TableFormComponent {
       (this.tableForm.value.tableAttributes as McdTableAttribute[])
         .map((attribute: Attribute) => attribute.attributeName));
     this.tableService.addTable(table);
+  }
+
+  attributeExistsValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (!control.value) return null;
+      for (let aControl of this.attributes.controls) {
+        if (aControl.value.attributeName == control.value) {
+          return { attributeExists: `L'attribut ${control.value} existe déjà` };
+        }
+      }
+      return null;
+    }
   }
 }
